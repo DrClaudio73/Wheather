4ebd6f5139ea61f1eea0963327ad161450bcb824
DrClaudio73 <ccattaneo73@gmail.com>
2018-12-30 12:51:34 +0100

    avoid reallocation of listening socket

--- Wheather/Wheather.py
+++ Wheather/Wheather.py
@@ -32,22 +32,24 @@ def updateWheateherValues(boardType,counterK,tempoalboot):
     tempolocaleFormattato=[]
     tempolocaleFormattato.append(str(tloc[0])+"/"+str(tloc[1])+"/"+str(tloc[2]))
     tempolocaleFormattato.append(str(tloc[3])+":"+str(tloc[4])+":"+str(tloc[5]))
-    stringaHtmlDati_loc='            <tr><td>%d</td><td>%d</td><td>%s</td><td>%s</td><td>%2.1f</td><td>%2.1f</td><td>%2.1f</td></tr>' % (counterK, tempodareboot,tempolocaleFormattato[0],tempolocaleFormattato[1],dsTemp, dh11_Temp,dh11Hum) +'\n'
+    stringaHtmlDati_loc='            <tr><td>%d</td><td>%d</td><td>%s</td><td>%s</td><td>%2.1f</td><td>%d</td><td>%d</td></tr>' % (counterK, tempodareboot,tempolocaleFormattato[0],tempolocaleFormattato[1],dsTemp, dh11_Temp,dh11Hum) +'\r\n'
     dati_loc=str(counterK)+";"+str(tempodareboot)+";"+str(tempolocaleFormattato[0])+";"+str(tempolocaleFormattato[1])+";"+("%2.1f" % dsTemp)+";"+str(dh11_Temp)+";"+str(dh11Hum)+"\r\n"
     return stringaHtmlDati_loc, dati_loc
 
-def startWebServer(boardType):
+def startWebServer(boardType,s,addrListen,ResetCsvFile=False):
+    print("entering setWebServer",boardType,s,addrListen)
+    '''
     try:
         addrListen = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(mainAppConstants.SOCKETTIMEOUT)
         s.bind(addrListen)
         s.listen(1)
-
     except:
         print("EXCEPTION WHILE ALLOCATING SOCKET")
-        return -1
-
+        s.close()
+        return -1,ResetCsvFile
+    '''
     while True:
         try:
             print('listening on', addrListen)
@@ -56,10 +58,9 @@ def startWebServer(boardType):
             print('client connected from:', addr)
         except:
             print("Timeout expired")
-            s.close()
-            #cliente.close()
-            return -2
-        
+            #s.close()
+            #cliente.close() no put this line otherwise local variable referenced before assignement
+            return -2,ResetCsvFile
         try:
             request = cliente.recv(1024)
             request = str(request)
@@ -67,9 +68,9 @@ def startWebServer(boardType):
             print("richiesta main loop:",parseRequest(str(request)))
         except:
             print("EXCEPTION OCCURRED WHILE RECEIVING DATA FROM SOCKET")
-            s.close()
+            #s.close()
             cliente.close()
-            return -3
+            return -3,ResetCsvFile
         
         #Sending response
         try:
@@ -95,6 +96,21 @@ def startWebServer(boardType):
                         cliente.send(x)
                 fContentToSend.close()
                 cliente.close()
+            elif parseRequest(str(request))=="clean_csv": #sending message that csv file will be reset
+                ResetCsvFile=True
+                cliente.send('HTTP/1.1 200 OK')
+                cliente.send('Server: LiteSpeed')
+                cliente.send('Expires: Sat, 28 Nov 2019 05:36:25 GMT')
+                cliente.send('Content-Type: text/html; charset=UTF-8')
+                cliente.send('Last-Modified: Sat, 28 Nov 2018 19:50:37 GMT')
+                cliente.send('Cache-Control: no-cache')
+                cliente.send('Connection: close\r\n\r\n')
+                cliente.send('<!DOCTYPE html> <html> <head> <title>ESP32 Reading Temperature and Humidity values</title> </head>')
+                cliente.send('    <body>\n        <h1>OK file will be reset!!!!</h1>\n')
+                cliente.send('        <a href="/"><button class="button">RETURN!</button></a>\n')
+                cliente.send('\n    </body>\n</html>')
+                cliente.close()
+                return 0,ResetCsvFile
             else: #sending HTML page
                 if boardType=='esp32':
                     fHeadersToSend=open('headers_main_pageESP32.txt','r')
@@ -112,14 +128,16 @@ def startWebServer(boardType):
                 #fContentToSend.close()
                 
                 #cliente.send(str(lunghezza)+'\r\n\r\n')
-                
+                cliente.send('\r\n')
                 fContentToSend=open('main_out.txt','r')
                 with fContentToSend:
                     for x in fContentToSend:
                         cliente.send(x)
                 fContentToSend.close()
 
-                cliente.send('        </table>\n        <a href="/dati.csv"><button class="button button2">GET LOG FILE!</button></a>\n        <h2>Your request was</h2>\n        <code>')
+                cliente.send('        </table>\n        <a href="/dati.csv"><button class="button button2">GET LOG FILE!</button></a>')
+                cliente.send('\n        <a href="/clean_csv"><button class="button">CLEAN LOG FILE!</button></a>')
+                cliente.send('\n        <h2>Your request was</h2>\n        <code>')
                 cliente.send(request)
                 cliente.send("</code>\n")
 
@@ -128,35 +146,77 @@ def startWebServer(boardType):
                     for x in fContentToSend:
                         cliente.send(x)
                 fContentToSend.close()
-
                 cliente.close()
         except:
             print("EXCEPTION OCCURRED WHILE SENDING DATA ON SOCKET")
-            s.close()
+            #s.close()
             cliente.close()
-            return -4
+            return -4,ResetCsvFile
 
 def main(boardType,tempoalboot):
     print("\n=====================================\nWelcome to Wheather App on "+str(boardType)+".....\n=====================================\n")
     counterK=0
+    ResetCsvFile=False
+    wait=0
+    try:
+        addrListen = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(mainAppConstants.SOCKETTIMEOUT)
+        print("mainAppConstants.SOCKETTIMEOUT",mainAppConstants.SOCKETTIMEOUT)
+        s.bind(addrListen)
+        s.listen(1)
+        print("first allocation",s.fileno())
+    except:
+        wait=1
+        s.close()
+        print('errore in first socket allocation')
+
     while True:
+        wait=0
+        try:
+            if s.fileno()==-1:
+                print('file no = -1')
+                addrListen = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.settimeout(mainAppConstants.SOCKETTIMEOUT)
+                s.bind(addrListen)
+                s.listen(1)
+                print("loop allocation",s.fileno())
+            else:
+                print('else file no = ',s.fileno())
+        except:
+            print("EXCEPTION WHILE ALLOCATING SOCKET")
+            wait=1
+            s.close()
+            #return -1,ResetCsvFile
+
         #After MAX_SAMPLES_STORED samples
         if counterK == 0:
-            #resets the content of files DA VALUTARE SE RESETTARLO QUESTO
+            #resets the content of html page
             fout=open('main_out.txt','w')
-            #fin=open('headers_main_page.txt','r')
-            #for x in fin:
             fout.write('')
             fout.close()
-            #fin.close()
 
-            fout=open('dati.csv','w')
+            #starts new section in csv file
             fin=open('headers_download_file.txt','r')
+            fout=open('dati.csv','a')
             for x in fin:
                 fout.write(x)
             fout.close()
             fin.close()
 
+        if ResetCsvFile:
+            fout=open('main_out.txt','w')
+            fout.write('')
+            fout.close()
+
+            fin=open('headers_download_file.txt','r')
+            fout=open('dati.csv','w')
+            for x in fin:
+                fout.write(x)
+            fout.close()
+            fin.close()
+            ResetCsvFile=False
         #Getting Samples
         stringaHtmlDati, dati=updateWheateherValues(boardType,counterK,tempoalboot)
         #Updating html main page
@@ -180,7 +240,8 @@ def main(boardType,tempoalboot):
         #verifies if in the meanwhile WiFiSTA came up
         activateNet.do_setupWiFiSTA()
 
-        retCode=startWebServer(boardType)
-        if retCode == -1:
+        retCode,ResetCsvFile=startWebServer(boardType,s,addrListen)
+        if wait == -1:
+            print('\nWaiting for TIME SLEEP.....\n')
             utime.sleep(145)
     #no_debug()
